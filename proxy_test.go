b96f79d4c54514e107811dae85a2b4547158a768
lovedboy <admin@lovedboy.com>
2017-08-03 09:33:30 +0800

    add more test

--- proxy_test.go
+++ proxy_test.go
@@ -4,8 +4,10 @@ import (
 	"golang.org/x/net/proxy"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
+	"sync"
 	"testing"
 	"time"
 )
@@ -39,16 +41,31 @@ func (h *handler) Clean() {
 
 }
 
-func TestServer_ListenAndServe(t *testing.T) {
+var (
+	start bool
+	lock  sync.Mutex
+)
 
+func startServer() {
+	lock.Lock()
+	defer lock.Unlock()
+	if start {
+		return
+	}
 	s := &Server{
 		Addr: ":12580",
 	}
-
 	h := &handler{}
 	s.HTTPHandler = h
+	s.Socks5Handler = h
 	go s.ListenAndServe()
 	time.Sleep(time.Millisecond * 100)
+	start = true
+}
+
+func TestServer_HTTP(t *testing.T) {
+
+	startServer()
 	proxyUrl, _ := url.Parse("http://127.0.0.1:12580")
 	myClient := &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)}}
 	res, err := myClient.Get("http://example.com")
@@ -61,24 +78,89 @@ func TestServer_ListenAndServe(t *testing.T) {
 	if string(body) != "hello,world" {
 		t.Fatal("http proxy err")
 	}
+}
 
-	s.Socks5Handler = h
-	dialer, err := proxy.SOCKS5("tcp", ":12580", nil, proxy.Direct)
+func TestServer_HTTPCONNECT(t *testing.T) {
+
+	startServer()
+	conn, err := net.Dial("tcp", "127.0.0.1:12580")
+	if err != nil {
+		t.Fatal(err)
+	}
+	conn.Write([]byte("CONNECT qq.com:443 HTTP/1.1\r\n\r\n"))
+	buf := make([]byte, 1024)
+	_, err = io.ReadAtLeast(conn, buf, len("HTTP/1.1 200 Connection Established\r\n\r\n"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	body, _ := ioutil.ReadAll(conn)
+	if string(body) != msg {
+		t.Fatal("http connect err")
+	}
+}
+
+func TestServer_Socks5Domain(t *testing.T) {
+	startServer()
+	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:12580", nil, proxy.Direct)
 	if err != nil {
 		t.Fatal(err)
 	}
 	httpTransport := &http.Transport{}
 	httpTransport.Dial = dialer.Dial
-	myClient = &http.Client{Transport: httpTransport}
+	myClient := &http.Client{Transport: httpTransport}
 
-	res, err = myClient.Get("http://example.com")
+	res, err := myClient.Get("http://example.com")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer res.Body.Close()
-	body, _ = ioutil.ReadAll(res.Body)
+	body, _ := ioutil.ReadAll(res.Body)
 	t.Logf("body:%s", string(body))
 	if string(body) != "hello,world" {
-		t.Fatal("socks proxy err")
+		t.Fatal("socks domain err")
+	}
+}
+
+func TestServer_Socks5IPV4(t *testing.T) {
+	startServer()
+	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:12580", nil, proxy.Direct)
+	if err != nil {
+		t.Fatal(err)
+	}
+	httpTransport := &http.Transport{}
+	httpTransport.Dial = dialer.Dial
+	myClient := &http.Client{Transport: httpTransport}
+
+	res, err := myClient.Get("http://8.8.8.8")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	t.Logf("body:%s", string(body))
+	if string(body) != "hello,world" {
+		t.Fatal("socks ipv4 err")
+	}
+}
+
+func TestServer_Socks5IPV6(t *testing.T) {
+	startServer()
+	dialer, err := proxy.SOCKS5("tcp", "127.0.0.1:12580", nil, proxy.Direct)
+	if err != nil {
+		t.Fatal(err)
+	}
+	httpTransport := &http.Transport{}
+	httpTransport.Dial = dialer.Dial
+	myClient := &http.Client{Transport: httpTransport}
+
+	res, err := myClient.Get("http://[::1]:12345")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	t.Logf("body:%s", string(body))
+	if string(body) != "hello,world" {
+		t.Fatal("socks ipv6 err")
 	}
 }
